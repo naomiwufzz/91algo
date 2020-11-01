b3cf1029a49949bc936e123d389d06a76406f310
naomiwufzz <47736085+naomiwufzz@users.noreply.github.com>
2020-11-01 18:29:32 +0800

    把全是9需要加一位的情况放在最后检查和添加

--- day1_plus_one.py
+++ day1_plus_one.py
@@ -2,28 +2,25 @@ class Solution:
     def plusOne(self, digits):
         array_length = len(digits)
 
-        for i in range(array_length-1, -1, -1):
+        for i in range(array_length-1, -1, -1): # -1的时候把数组长度为1的情况和别的情况合并，每种情况都不会遍历到-1
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-
             else:
                 digits[i] = 0
-                if digits[i-1] < 9:
+                if i-1 != -1 and digits[i-1] < 9: # 对i-1是-1的情况进行拦截
                     digits[i-1] += 1
                     return digits
                 else:
-                    if i-1 == 0 and digits[i-1] == 9:
-                        digits[0] = 0
-                        digits.insert(0, 1)
-                        return digits
-                    else:
-                        continue
+                    continue
+        if digits[0] == 0:
+            digits.insert(0,1)
+        return digits
 
 
 if __name__ == '__main__':
     sol = Solution()
-    inputs = [[1, 2, 3], [1, 2, 9], [9, 9, 9], [0], [1, 9, 8]]
+    inputs = [[1, 2, 3], [1, 2, 9], [9, 9, 9], [0], [1, 9, 8], [1, 9], [9]]
     for input in inputs:
         result = sol.plusOne(input)
         print(result)
